43c9faa367959b6178644a84eb638c14cb4b354e
PalermoD <palermo.deschamps@gmail.com>
2015-11-01 15:03:14 -0500

    contact page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,4 +10,7 @@ class StaticPagesController < ApplicationController
 
   def portfolio
   end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'portfolio' => 'static_pages#portfolio'
 
+  get 'contact' => 'static_pages#contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
